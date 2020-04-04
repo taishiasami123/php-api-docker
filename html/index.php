23fb26fd08d4d94b8c611745977fd36bc76cd559
taishiasami123 <taishi.asami123@gmail.com>
2020-04-04 20:45:03 +0900

    URLから文字列を取り出す機能を追加

--- html/index.php
+++ html/index.php
@@ -3,20 +3,20 @@
   header('Access-Control-Allow-Headers: *');
   header('Access-Control-Allow-Methods: *');
 
-  $_GET['page'] = 1;
-  $_GET['limit'] = 1;
-  $_GET['query'] = 1;
-
   // jsonを返して死ぬやつ
   function sendResponse($obj) {
     echo json_encode($obj);
     die();
   }
 
-  // $requestUri = $_SERVER['REQUEST_URI'];
-  // if (strpos($requestUri, '?') != false) {
-  //   $paramsExcludedUri = split('?', $requestUri);
-  // }
+  // URLから各機能の文字列を取り出す
+  $requestUri = $_SERVER['REQUEST_URI'];
+  if (strpos($requestUri, '?') != false) {
+    $paramsExcludedUri = explode('?', $requestUri)['0'];
+  } else {
+    $paramsExcludedUri = $requestUri;
+  }
+  $substrUri = substr($paramsExcludedUri, 9, strlen($paramsExcludedUri) - 9);
 
   sendResponse('test');
 ?>
